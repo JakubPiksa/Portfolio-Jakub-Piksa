bd8ea060a8207f0a7a3b5a5c25de69562f0ec980
Jakub Piksa <kuba.piksa@wp.pl>
2023-09-20 12:38:46 +0200

    Routing fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import Portfolio from './Portfolio/Portfolio';
 import Nav from './Nav/Nav';
@@ -9,18 +9,22 @@ import css from './app.module.css';
 
 const App = () => {
   return (
-    <Router >
-      <div className={css.gradientbackground}>
-        <h1 className={css.h1}>Portfolio Jakub Piksa </h1>
-        <h2 className={css.h2}>WORK IN PROGRESS</h2>
-        <Nav />
-        <Routes>
+    
+    <div className={css.gradientbackground}>
+      
+      <h1 className={css.h1}>Portfolio Jakub Piksa </h1>
+      <h2 className={css.h2}>WORK IN PROGRESS</h2>
+      
+      <Nav />
+      
+      <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+      </Routes>
+      
+    </div>
+   
   );
 };
 
