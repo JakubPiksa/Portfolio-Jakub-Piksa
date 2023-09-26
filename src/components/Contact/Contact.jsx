e64cae3b8a84e64268087d0cca506a1a0c22e591
Jakub Piksa <kuba.piksa@wp.pl>
2023-09-26 21:35:58 +0200

    contacts update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import css from './contact.module.css';
-
+import LinkedIn from '../../utils/icons/linkedin.svg'
+import Mail from '../../utils/icons/envelope.svg'
+import GitHub from '../../utils/icons/github.svg'
+import Phone from '../../utils/icons/phone.svg'
+ 
 const Contact = () => {
   return (
     <div className={css.contactContainer}>
       <h1>Contact Me</h1>
       <div className={css.contactInfo}>
-        <div className={css.contactItem}>
-          <img src="path/to/github-icon.png" alt="GitHub" />
-          <a href="https://github.com/jakubpiksa">GitHub</a>
+        <div className={css.contactItem}>          
+          <a href="https://github.com/jakubpiksa"><img src={GitHub} alt="GitHub" className={css.contactItemImg} />GitHub</a>
         </div>
-        <div className={css.contactItem}>
-          <img src='../../utils/iconsP.svg#icon-linkedin'></img>
-          <a href="https://www.linkedin.com/in/jakub-piksa-318b37245/">LinkedIn</a>
+        <div className={css.contactItem}>          
+          <a href="https://www.linkedin.com/in/jakub-piksa-318b37245/"><img src={LinkedIn} alt='linkedin' className={css.contactItemImg}></img>LinkedIn</a>
         </div>
-        {/* Add more contact items (email, phone, etc.) */}
+        <div className={css.contactItem}>          
+          <a href="mailto:kuba.piksa@wp.pl"><img src={Mail} alt='E-Mail' className={css.contactItemImg}></img>E-mail</a>
+        </div>
+        <div className={css.contactItem}>          
+          <a href="tel:+48690314155"><img src={Phone} alt='690 314 155' className={css.contactItemImg}></img>Call Me</a>
+        </div>
+        
       </div>
     </div>
   );
