a88c3f0148e1ccba2ff4146fd2ffee61d2ce3f03
Jakub Piksa <kuba.piksa@wp.pl>
2023-10-01 22:41:19 +0200

    About me Start

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,12 @@ import css from './about.module.css'
 
 const About = () => {
     return (
-        <h1 className={css.header}>WORK IN PROGRES About ME</h1>
+        <div>
+            <h1 className={css.header}>WORK IN PROGRES About ME</h1>
+            <div className={css.textContainer}>
+                <p className={css.text}>My name is Jakub Piksa, and for over a year, I have been actively and enthusiastically pursuing my passion for programming. I am a student in the Full Stack Development program led by GoIT, and it's here that I took my first serious steps in this field. On a daily basis for the last 2.5 years, I've been involved in the assembly of joinery, both within the country and abroad. This experience taught me teamwork under various conditions and time pressure, as well as fruitful collaboration and negotiation with clients. I am continuously expanding and honing my skills in work organization, project management, and engineering knowledge and practice through my part-time studies in management engineering. I completed my technical high school with a title of logistics technician, and I apply the knowledge I gained there to organize my work. In my daily life, besides programming, I've been passionate about various sports, from the gym and martial arts to skating, since childhood. Along with my wife, we discovered a common passion for traveling, which involves experiencing and learning about different cultures. I am a hardworking and determined individual, committed to reaching my goals and enthusiastic about life. My motto is the belief that every transformation and achievement starts in our minds, so with discipline, passion, and dedication, there's nothing we can't accomplish. I am excited and eager for further development and new challenges in the field of IT.</p>
+            </div>
+        </div>
     )
 }
    
